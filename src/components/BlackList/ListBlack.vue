c0d258802dfddf985832755d95084abb1f3f0f62
Arlekin <frothen-trone@mail.ru>
2024-04-02 13:51:48 +0500

    tablesort

--- src/components/BlackList/ListBlack.vue
+++ src/components/BlackList/ListBlack.vue
@@ -1,27 +1,25 @@
 <script>
-import { ref, computed } from 'vue';
-
 export default {
   data() {
     return {
       sortKey: '',
       search: '',
       reverse: false,
-      columns: ['ID', 'IP адрес', 'age'],
+      columns: ['ID', 'IP', 'Date', 'Browser', 'OS', 'Country', 'Type'],
       people: [
-        { name: 'John', age: 50 },
-        { name: 'Jack', age: 35 },
-        { name: 'Keith', age: 28 },
-        { name: 'Alain', age: 17 },
-        { name: 'Neil', age: 1 },
-        { name: 'Mark', age: 72 },
-        { name: 'Don', age: 47 },
-        { name: 'Walter', age: 41 },
-        { name: 'Jessy', age: 33 },
-        { name: 'Henck', age: 22 },
-        { name: 'Sal', age: 9 },
-        { name: 'Skyler', age: 42 },
-        { name: 'Holly', age: 55 },
+        { ID: 0, IP: '127.0.0.1', Date: '10.05.2024', Browser: 'Unkown', OS: 'Windows 7', Country: 'United State', Type: '</>SQLI' },
+        { ID: 3, IP: '127.0.0.2', Date: '10.05.2023', Browser: 'Unkown', OS: 'Windows 8', Country: 'United State', Type: '</>SQLI' },
+        { ID: 5, IP: '127.0.0.5', Date: '10.05.2021', Browser: 'Unkown', OS: 'Windows 7', Country: 'United State', Type: '</>SQLI' },
+        { ID: 87, IP: '127.0.0.7', Date: '10.05.2020', Browser: 'Unkown', OS: 'Windows 9', Country: 'United State', Type: '</>SQLI' },
+        { ID: 12, IP: '127.0.0.8', Date: '10.05.2025', Browser: 'Unkown', OS: 'Windows 7', Country: 'United State', Type: '</>SQLI' },
+        { ID: 1, IP: '127.0.0.9', Date: '10.05.2026', Browser: 'Unkown', OS: 'Windows 7', Country: 'Brazil', Type: '</>SQLI' },
+        { ID: 6, IP: '127.0.0.10', Date: '10.05.2028', Browser: 'Unkown', OS: 'Windows 7', Country: 'Kanada', Type: '</>SQLI' },
+        { ID: 2, IP: '127.0.0.12', Date: '10.05.2027', Browser: 'Unkown', OS: 'Windows 7', Country: 'United State', Type: '</>SQLI' },
+        { ID: 4, IP: '127.0.0.15', Date: '10.05.2018', Browser: 'Unkown', OS: 'Windows 7', Country: 'Spanish', Type: '</>SQLI' },
+        { ID: 9, IP: '127.0.0.17', Date: '10.05.2019', Browser: 'Unkown', OS: 'Windows 7', Country: 'Italia', Type: '</>SQLI' },
+        { ID: 10, IP: '127.0.0.19', Date: '10.05.2016', Browser: 'Unkown', OS: 'Windows 7', Country: 'France', Type: '</>SQLI' },
+        { ID: 11, IP: '127.0.0.13', Date: '10.05.2015', Browser: 'Unkown', OS: 'Windows 7', Country: 'Germany', Type: '</>SQLI' },
+        { ID: 7, IP: '127.0.0.14', Date: '10.05.2014', Browser: 'Unkown', OS: 'Windows 7', Country: 'Great Britan', Type: '</>SQLI' },
       ],
     };
   },
@@ -34,7 +32,7 @@ export default {
   computed: {
     sortedPeople() {
       return this.people
-        .filter((person) => person.name.toLowerCase().includes(this.search.toLowerCase()))
+        .filter((person) => person.ID.toString().includes(this.search.toLowerCase()))
         .sort((a, b) => {
           const modifier = this.reverse ? -1 : 1;
           if (a[this.sortKey] < b[this.sortKey]) return -1 * modifier;
@@ -47,47 +45,52 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-      <div id="vue-table">
-        <input type="text" v-model="search" class="form-control" />
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th v-for="column in columns" :key="column">
-                <a href="#" @click="sortBy(column)" :class="{ active: sortKey === column }">
-                  {{ column }}
-                </a>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="person in sortedPeople" :key="person.name">
-              <td>{{ person.name }}</td>
-              <td>{{ person.age }}</td>
-              <td>{{ person.age }}</td>
-              <td><Button>bu</Button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+  <div class="container">
+    <div id="vue-table">
+      <input type="text" v-model="search" class="form-control" />
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th v-for="column in columns" :key="column">
+              <a href="#" @click="sortBy(column)" :class="{ active: sortKey === column }">
+                {{ column }}
+              </a>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="person in sortedPeople" :key="person.ID">
+            <td>{{ person.ID }}</td>
+            <td>{{ person.IP }}</td>
+            <td>{{ person.Date }}</td>
+            <td>{{ person.Browser }}</td>
+            <td>{{ person.OS }}</td>
+            <td>{{ person.Country }}</td>
+            <td>{{ person.Type }}</td>
+            <td><a href="#">Детяли</a><a href=""></a><a href=""></a></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  </template>
-  
-  <style scoped>
-  #vue-table {
-    margin: 2em 0;
-    background: #343549;
-  }
-  
-  #vue-table a {
-    font-weight: bold;
-    text-decoration: none;
-  }
-  
-  #vue-table a.active {
-    font-weight: bold;
-    color: black;
-    text-decoration: underline;
-  }
-  </style>
-  
+  </div>
+</template>
+
+<style scoped>
+.container{
+  background: #343549;  
+}
+#vue-table {
+  margin: 2em 0;
+}
+
+#vue-table a {
+  font-weight: bold;
+  text-decoration: none;
+}
+
+#vue-table a.active {
+  font-weight: bold;
+  color: black;
+  text-decoration: underline;
+}
+</style>
